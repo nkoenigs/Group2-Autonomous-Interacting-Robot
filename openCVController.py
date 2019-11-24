617850d169e13ce3bbba18b18995adbf2c6b9802
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2019-11-23 19:38:27 -0500

    Update openCVController.py

--- openCVController.py
+++ openCVController.py
@@ -504,7 +504,7 @@ def run(cvQueue: Queue):
             elif commandFromQueue == "aprilFollow":
                 # We know the next 2 items in the queue in this case are the x and z coordinates - grab them
                 #  Note: get() commands will block until it can get something
-                cvQueue.put("Receive april Tag request")
+                print("Receive april Tag request")
                 target_tag_number = cvQueue.get()
                 target_tag_radius = cvQueue.get()
                 cvObject.april_following(target_tag_number, target_tag_radius, cvQueue)
